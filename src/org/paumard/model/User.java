947d30d23419a44018866711d955082f82899f93
Sunil <sunil1863@gmail.com>
2021-04-17 16:31:36 +0530

    Added getter and setter for name field.

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -2,4 +2,14 @@ package org.paumard.model;
 
 public class User {
 
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
 }
